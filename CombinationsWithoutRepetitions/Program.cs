4548fb281eb0c852463e05772ba603ca1160c039
velislav1 <velislav1@students.softuni.bg>
2021-11-17 16:13:09 +0200

    CombinationsWithoutRepetitions

---
+++ CombinationsWithoutRepetitions/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace CombinationsWithoutRepetitions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            var k = int.Parse(Console.ReadLine());
+
+            var set = new int[n];
+
+            var sequence = new int[k];
+
+            Generate(set, sequence, 0, 1);
+
+        }
+
+        public static void Generate(int[] set, int[] seq, int index, int loopStart)
+        {
+
+            if (index == seq.Length)
+            {
+
+                Console.WriteLine(string.Join(" ", seq));
+                return;
+            }
+
+            for (int i = loopStart; i <= set.Length; i++)
+            {
+                seq[index] = i;
+
+                Generate(set, seq, index + 1, i + 1);
+
+            }
+
+        }
+
+    }
+}
