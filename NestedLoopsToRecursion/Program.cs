5844fbe5bf04e480799f20e0d8a8a022b00b1e30
velislav1 <velislav1@students.softuni.bg>
2021-11-17 12:33:07 +0200

    NestedLoopsToRecursion

---
+++ NestedLoopsToRecursion/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NestedLoopsToRecursion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            var sequence = new int[n];
+
+            PrintSequence(n, sequence, 0);
+        }
+
+
+        public static void PrintSequence(int n, int[] sequence, int index)
+        {
+
+            if(index == n)
+            {
+                Console.WriteLine(string.Join(" ", sequence));
+                return;
+            }
+
+            for (int i = 1; i <= n; i++)
+            {
+                sequence[index] = i;
+
+                PrintSequence(n, sequence, index + 1);
+
+            }
+
+        }
+
+    }
+}
